bb735cfa859a006b6b29fabb0827be523a5a7512
Misba Khanum <misbakhan5922@gmail.com>
2025-10-30 18:21:25 +0530

    Remove unnecessary comments in AdvisorController

--- src/main/java/com/wellsfargo/counselor/controller/AdvisorController.java
+++ src/main/java/com/wellsfargo/counselor/controller/AdvisorController.java
@@ -15,31 +15,31 @@ public class AdvisorController {
     @Autowired
     private AdvisorService advisorService;
 
-    // Get all advisors
+    
     @GetMapping
     public List<Advisor> getAllAdvisors() {
         return advisorService.getAllAdvisors();
     }
 
-    // Get advisor by ID
+    
     @GetMapping("/{id}")
     public Optional<Advisor> getAdvisorById(@PathVariable Long id) {
         return advisorService.getAdvisorById(id);
     }
 
-    // Create a new advisor
+    
     @PostMapping
     public Advisor createAdvisor(@RequestBody Advisor advisor) {
         return advisorService.createAdvisor(advisor);
     }
 
-    // Update advisor by ID
+    
     @PutMapping("/{id}")
     public Advisor updateAdvisor(@PathVariable Long id, @RequestBody Advisor updatedAdvisor) {
         return advisorService.updateAdvisor(id, updatedAdvisor);
     }
 
-    // Delete advisor by ID
+    
     @DeleteMapping("/{id}")
     public void deleteAdvisor(@PathVariable Long id) {
         advisorService.deleteAdvisor(id);
